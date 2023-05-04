e599570c6635df7108a8a78b99dbbf55e79384a0
Vishal B B <vbb@BLR.ALLEGISINDIA.COM>
2023-05-04 10:58:17 +0530

    Hello

---
+++ src/main/java/com/example/SpringBootSampleDemo/SpringBootSampleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSampleDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootSampleDemo/SpringBootSampleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSampleDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSampleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
